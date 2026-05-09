48937da1c47ba6a0f379559554542a413d1179df
dungngminh <63831488+dungngminh@users.noreply.github.com>
2026-05-10 03:43:16 +0700

    update config and animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://submindo-web.vercel.app',
   integrations: [sitemap()],
+  vite: {
+    build: {
+      sourcemap: false,
+    },
+  },
 });
